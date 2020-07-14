c6e8b965670fefe8dd00c78c38ec5555f9a3b204
Jason Sonnichsen <Harliquinn@gmail.com>
2020-07-14 17:57:24 -0400

    Explicitly typed variables

--- sandbox.js
+++ sandbox.js
@@ -1,30 +1,17 @@
-//arrays
-var names = ['Luigi', 'Mario', 'Yoshi'];
-names.push('Toad');
-//ERROR
-// names.push(3);
-// names[0]=3;
-var numbers = [10, 20, 30, 40];
-numbers.push(25);
-//ERROR
-// numbers.push('Shaun');
-var mixed = ['Ken', 4, 'Chun-Li', 8, 9];
-mixed.push('Ryu');
-mixed[0] = 'Hi';
-//ERROR
-// mixed.push(true);
-//objects
-var ninja = {
-    name: 'Mario',
-    belt: 'black',
-    age: 30
-};
-ninja.age = 40;
-//ERROR
-// ninja.age.age='30';
-// ninja.skills=['Fighting','Sneaking'];
-ninja = {
-    name: 'Yoshi',
-    belt: 'orange',
-    age: 40
-};
+//Explicit Types
+var characer;
+var age;
+var isLoggedIn;
+//Arrays
+var ninjas = [];
+//Union Types
+var mixed = [];
+mixed.push(10);
+mixed.push('Blah');
+console.log(mixed);
+var uid;
+//Objects
+var ninjaOne;
+ninjaOne = { name: 'Yoshi', age: 30 };
+ninjaOne = [];
+var ninjaTwo;
--- sandbox.ts
+++ sandbox.ts
@@ -1,39 +1,28 @@
-//arrays
-let names = ['Luigi','Mario','Yoshi'];
-names.push('Toad');
-//ERROR
-// names.push(3);
-// names[0]=3;
+//Explicit Types
+let characer:string;
+let age:number;
+let isLoggedIn:boolean;
 
-let numbers=[10,20,30,40];
-numbers.push(25);
+//Arrays
+let ninjas:string[]=[];
 
-//ERROR
-// numbers.push('Shaun');
+//Union Types
+let mixed:(string|number)[]=[];
 
-let mixed = ['Ken',4,'Chun-Li',8,9];
+mixed.push(10);
+mixed.push('Blah');
+console.log(mixed);
 
-mixed.push('Ryu');
-mixed[0]='Hi';
+let uid:string|number;
 
-//ERROR
-// mixed.push(true);
+//Objects
+let ninjaOne:object;
 
-//objects
+ninjaOne={name:'Yoshi',age:30};
+ninjaOne=[];
 
-let ninja = {
-    name:'Mario',
-    belt: 'black',
-    age:30
-}
-
-ninja.age=40;
-//ERROR
-// ninja.age.age='30';
-// ninja.skills=['Fighting','Sneaking'];
-
-ninja={
-    name:'Yoshi',
-    belt:'orange',
-    age:40
-}
+let ninjaTwo:{
+    name:string,
+    age: number,
+    beltColour: string
+};
