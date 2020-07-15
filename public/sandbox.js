fca293717ff705366ff7e40266ea8cabe33f3880
Jason Sonnichsen <Harliquinn@gmail.com>
2020-07-15 15:05:40 -0400

    Function parameter type setting return value types optional arguments

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,2 +1,15 @@
 "use strict";
-console.log("test Blah");
+let greet;
+greet = () => {
+    console.log("Hello, again");
+};
+const add = (a, b, c) => {
+    console.log(a + b);
+    console.log(c);
+};
+add(5, 10, 20);
+const minus = (a, b) => {
+    return a + b;
+};
+let result = minus(10, 7);
+console.log(result);
